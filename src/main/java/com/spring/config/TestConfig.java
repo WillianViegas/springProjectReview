3a30ca9f85f8c798adcc63d3a3e16d58d22f79e2
Will <willianwed98@gmail.com>
2020-06-30 18:40:11 -0300

    Spring project

---
+++ src/main/java/com/spring/config/TestConfig.java
@@ -0,0 +1,93 @@
+package com.spring.config;
+
+import java.time.Instant;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.spring.entities.Category;
+import com.spring.entities.Order;
+import com.spring.entities.OrderItem;
+import com.spring.entities.Payment;
+import com.spring.entities.Product;
+import com.spring.entities.User;
+import com.spring.entities.enums.OrderStatus;
+import com.spring.repositories.CategoryRepository;
+import com.spring.repositories.OrderItemRepository;
+import com.spring.repositories.OrderRepository;
+import com.spring.repositories.ProductRepository;
+import com.spring.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category c1 = new Category(null, "Eletronics");
+		Category c2 = new Category(null, "Books");
+		Category c3 = new Category(null, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(c2);
+		p2.getCategories().add(c1);
+		p3.getCategories().add(c3);
+		p4.getCategories().add(c3);
+		p5.getCategories().add(c2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		
+		User u1 = new User(null, "Joao", "joao@gmail.com", "388348598", "pass123");
+		User u2 = new User(null, "Maria", "maria@gmail.com", "3434222", "1234");
+		User u3 = new User(null, "Marco", "marco@gmail.com", "3435221", "pass");
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-06-20T19:54:07Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-06-20T19:54:07Z"), OrderStatus.CANCELED, u1);
+		
+		userRepository.saveAll(Arrays.asList(u1,  u2, u3));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+	}
+	
+	
+}
---
+++ src/main/java/com/spring/services/OrderService.java
@@ -0,0 +1,64 @@
+package com.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.spring.entities.Order;
+import com.spring.repositories.OrderRepository;
+import com.spring.services.exceptions.DatabaseException;
+import com.spring.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Order insert(Order order) {
+		return repository.save(order);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);			
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Order update(Long id, Order obj) {
+		try {
+			Order entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Order entity, Order obj) {
+		entity.setOrderStatus(obj.getOrderStatus());
+		entity.setMoment(obj.getMoment());
+	}
+}
